3fb4af7c3247c9721aeda6aacc062e0878eb9f8a
Ali Abdul Wahid <aly.dihaw96@gmail.com>
2022-12-01 21:46:11 +0700

    remove unnecessary

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,13 +12,7 @@ import { AppComponent } from './app.component';
 import { FooterModule } from '@layouts/footer/footer.module';
 import { CopyrightModule } from '@layouts/copyright/copyright.module';
 import { ToastModule } from 'primeng/toast';
-import { NgxOtpInputModule } from 'ngx-otp-input';
 
-import {
-  HighlightModule,
-  HIGHLIGHT_OPTIONS,
-  HighlightOptions,
-} from 'ngx-highlightjs';
 
 @NgModule({
   declarations: [
@@ -28,29 +22,14 @@ import {
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
-    HighlightModule,
     NavbarModule,
     FooterModule,
     CopyrightModule,
     ThemeModule,
     ToTopModule,
     ToastModule,
-    NgxOtpInputModule,
   ],
   providers: [
-    {
-      provide: HIGHLIGHT_OPTIONS,
-      useValue: <HighlightOptions>{
-        lineNumbers: true,
-        coreLibraryLoader: () => import('highlight.js/lib/core'),
-        themePath: 'node_modules/highlight.js/styles/github.css',
-        languages: {
-          typescript: () => import('highlight.js/lib/languages/typescript'),
-          css: () => import('highlight.js/lib/languages/css'),
-          xml: () => import('highlight.js/lib/languages/xml'),
-        },
-      },
-    },
   ],
   bootstrap: [AppComponent]
 })
